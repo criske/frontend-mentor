7dfb5bd77a1239d3888e9370b0791b5e49b02749
cristianpela <cristypela05@gmail.com>
2021-08-27 13:15:27 +0300

    top section and grid section

--- comparator/app.js
+++ comparator/app.js
@@ -6,57 +6,49 @@ $(document).ready(() => {
         dialog.showModal();
     });
 
-    var start = 0;
+    // $(window).on('beforeunload', function(){
+    //     // your logic here
+    //     console.log("reload");
+    // });
+
     $('#preview-slider-btn').draggable({
         axis: 'x',
         containment: "parent",
         zIndex: 100,
-        grid: [ 5, 1 ],
+        grid: [5, 1],
         iframeFix: true,
         drag: (e, ui) => {
             if (e.offsetX >= 0) {
                 const parent = ui.helper.parent();
-                const min = parent.offset().left;
-                const max =  parent.offset().left + parent.outerWidth();
+                const min = parent.offset().left + ui.helper.width();
+                const max = parent.offset().left + parent.outerWidth()  - ui.helper.width();
                 const curr = ui.helper.offset().left;
-               var clampOffsetX;
-              if(curr < min){
-                clampOffsetX = min;
-              }else if(curr >  max ){
-                  clampOffsetX =  max;
-              }else{
-                  clampOffsetX = curr;
-              }
-              const perc = (clampOffsetX -  min) / (max - min) * 100;
-              $('#preview-pane').width(`${perc }%`);
+                var clampOffsetX;
+                if (curr < min) {
+                    clampOffsetX = min;
+                } else if (curr > max) {
+                    clampOffsetX = max;
+                } else {
+                    clampOffsetX = curr;
+                }
+                const perc = (clampOffsetX - min) / (max - min) * 100;
+                $('#preview-pane').width(`${perc}%`);
             }
         }
     });
 
-    // $('#preview-slider-btn')
-    //     .mousedown(function (e) {
-    //         $(this).on('mousemove', function (e) {
-    //             var x = e.pageX;
-    //             var oldX = $(this).data('x') || x;
-    //             var diff = x - oldX;
-    //             var currLeft = $(this).offset().left;
-    //             $(this).offset({ left: currLeft + diff }); //revert the way
-    //             $('#preview-pane').width($('#preview-pane').width() + diff);
-    //             $(this).data('x', x); //store
-    //         });
-    //     })
-    //     .mouseup(function () {
-    //         $(this).off('mousemove');
-    //         $(this).removeData('x');
-    //     });
 
-    // $('#preview-pane > img').width($('#preview-pane').parent().width());//revert the way
-    // $('#preview-image').width($('main').width()); // Units are assumed to be pixels
-    // $('#preview-image').height($('main').height());
-    $('#solution-pane').height($('#preview-image').height());
-    $('#solution-pane').width($('#preview-image').width());
-    $('#ui-widget-content').width($('#preview-image').width());
-    $('main').width($('#preview-image').width());
-    $('main').height($('#preview-image').height());
-    // $('#ui-widget-content')
+    setTimeout(() => {
+        $('#solution-pane').height($('#preview-image').height());
+        $('#solution-pane').width($('#preview-image').width());
+        $('.ui-widget-content').width($('#preview-image').width());
+        $('main').width($('#preview-image').width());
+        $('main').height($('#preview-image').height());
+        $('#preview-image').attr("style","opacity:0.5; -moz-opacity:0.5; filter:alpha(opacity=50)");
+    }, 500);
+
+    $('#preview-image-opacity-slider').on('input', function() {
+        const v = $(this).val();
+        $('#preview-image').attr("style",`opacity:${v/100}; -moz-opacity:${v/100}; filter:alpha(opacity=${v})`);
+    });
 });
